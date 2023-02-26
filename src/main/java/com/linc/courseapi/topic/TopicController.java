e7f39c8949b7af633a7be002a239027b1fae90c7
Mukhammad Pardaboev <mukhammad.pardaboev@sap.com>
2023-02-26 20:48:56 +0500

    Spring Boot Quick Start 18 - Getting a single resource

--- src/main/java/com/linc/courseapi/topic/TopicController.java
+++ src/main/java/com/linc/courseapi/topic/TopicController.java
@@ -1,9 +1,9 @@
 package com.linc.courseapi.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +15,10 @@ public class TopicController {
   @RequestMapping("/topics")
   public List<Topic> getAllTopics() {
     return this.topicService.getAllTopics();
+  }
+
+  @RequestMapping("/topics/{id}")
+  public Topic getTopic(@PathVariable String id) {
+    return this.topicService.getTopic(id);
   }
 }
