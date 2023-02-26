6de42706f24496d5c3d03f29c66bc69ad0a6ab58
Mukhammad Pardaboev <mukhammad.pardaboev@sap.com>
2023-02-26 19:56:26 +0500

    Spring Boot Quick Start 17 - Creating a business service

---
+++ src/main/java/com/linc/courseapi/topic/TopicService.java
@@ -0,0 +1,18 @@
+package com.linc.courseapi.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+  private List<Topic> topics = Arrays.asList(
+      new Topic("spring", "Spring Framework", "Spring Framework Description"),
+      new Topic("java", "Core Java", "Core Java Description"),
+      new Topic("javascript", "JavaScript", "JavaScript Description"));
+
+  public List<Topic> getAllTopics() {
+    return this.topics;
+  }
+}
